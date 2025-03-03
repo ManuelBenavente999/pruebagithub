140a64c0c7fe13de4afd2e6056def790e3af9dfb
ManuelBenavente999 <manbegon1994@gmail.com>
2025-03-03 12:58:37 +0100

    sexto commit

--- ejemplo1.java
+++ ejemplo1.java
@@ -4,8 +4,7 @@ public class ejemplo1 {
     public static void main(String[] args) {
         System.out.println("Hola mundo 1");
         System.out.println("Segundo Hola mundo Guay");
-    
-        System.out.println("Cuarto commit en rama dev");
+        System.out.println("Sexto Commit");
     }
 
 }
