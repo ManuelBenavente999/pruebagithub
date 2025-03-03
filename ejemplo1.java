fe7b148dc0f3cb5a41eb2a2849457f9ffd995de9
ManuelBenavente999 <manbegon1994@gmail.com>
2025-03-03 12:46:40 +0100

    Segundo commit de prueba

--- ejemplo1.java
+++ ejemplo1.java
@@ -3,6 +3,7 @@ package pruebagithub;
 public class ejemplo1 {
     public static void main(String[] args) {
         System.out.println("Hola mundo 1");
+        System.out.println("Segundo Hola mundo Guay");
     }
 
 }
